b1c780833b9b0976dcb48e2249d2dd64285ab679
Anwesh43 <anweshthecool0@gmail.com>
2020-08-09 18:22:47 +0530

    adding CRSNode class

--- index.ts
+++ index.ts
@@ -133,3 +133,45 @@ class Animator {
         }
     }
 }
+
+class CRSNode {
+
+    state : State = new State()
+    prev : CRSNode 
+    next : CRSNode 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new CRSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawCRSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : CRSNode {
+        var curr : CRSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
