2d7215f71e0f65f243e877a8dfd74c996d7da34f
Anwesh43 <anweshthecool0@gmail.com>
2020-08-09 17:45:27 +0530

    adding constants and libraries

---
+++ index.ts
@@ -0,0 +1,11 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight
+const parts : number = 4  
+const scGap : number = 0.02 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 5.2 
+const colors : Array<string> = ["#3F51B5", "#4CAF50", "#F44336", "#FFEB3B", "#009688"]
+const delay : number = 20
+const backColor : string = "#bdbdbd"
+
+
