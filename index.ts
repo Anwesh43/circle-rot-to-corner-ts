e76d47d146a00781982395737a23df06ddd16ed0
Anwesh43 <anweshthecool0@gmail.com>
2020-08-09 18:25:40 +0530

    adding CircleRotSquare class

--- index.ts
+++ index.ts
@@ -175,3 +175,25 @@ class CRSNode {
         return this 
     }
 }
+
+class CircleRotSquare {
+    curr : CRSNode = new CRSNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
